1c45a8b88484b21b3afb6c92f0345a6bd4a6656b
David Catmull <dtcatmull@verisk.com>
2024-11-21 23:01:57 -0700

    Add Each test

--- Tests/ScaffTests/ScaffExecutionTests.swift
+++ Tests/ScaffTests/ScaffExecutionTests.swift
@@ -45,4 +45,37 @@ final class ScaffExecutionTests: XCTestCase {
     XCTAssert(didIt, "It did not execute")
     XCTAssert(didAfter, "AfterAll did not execute")
   }
+
+  func testBeforeAfterEach() throws {
+    var beforeCount = 0
+    var itCount = 0
+    var afterCount = 0
+
+    let test = Describe("Running each hooks") {
+      BeforeEach {
+        beforeCount += 1
+      }
+
+      It("runs first test") {
+        XCTAssertEqual(beforeCount, 1)
+        XCTAssertEqual(afterCount, 0)
+        itCount += 1
+      }
+
+      It("runs second test") {
+        XCTAssertEqual(beforeCount, 2)
+        XCTAssertEqual(afterCount, 1)
+        itCount += 1
+      }
+
+      AfterEach {
+        afterCount += 1
+      }
+    }
+
+    try test.execute()
+    XCTAssertEqual(beforeCount, 2, "BeforeEach didn't run correctly")
+    XCTAssertEqual(itCount, 2, "Its didn't run correctly")
+    XCTAssertEqual(afterCount, 2, "AfterEach didn't run correctly")
+  }
 }
