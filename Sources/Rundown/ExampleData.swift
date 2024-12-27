f9ed6c04ecdf5686da93751f269859466107c27e
David Catmull <davidcatmull@gmail.com>
2024-12-27 16:20:32 -0700

    Extract ExampleRun to its own file

--- Sources/Rundown/ExampleData.swift
+++ Sources/Rundown/ExampleData.swift
@@ -158,48 +158,7 @@ public struct Within: ExampleElement {
   }
 }
 
-/// Tracks the execution of an example group in order to construct the full name
-/// of the current element.
-public class ExampleRun: @unchecked Sendable {
-  // Manual lock for unchecked sendability
-  private let lock = NSRecursiveLock()
-  internal static let logger = Logger(subsystem: "Rundown", category: "ExampleRun")
 
-  var elementStack: [any Element] = []
-  var description: String {
-    withLock {
-      elementStack.map { $0.description }.joined(separator: ", ")
-    }
-  }
-
-  @TaskLocal
-  static var current: ExampleRun? = nil
-
-  internal init() {}
-
-  private func withLock<T>(_ block: () throws -> T) rethrows -> T {
-    lock.lock()
-    defer { lock.unlock() }
-    return try block()
-  }
-
-  func withElement(_ element: some Element, block: () throws -> Void) rethrows {
-    withLock { elementStack.append(element) }
-    try block()
-    withLock { _ = elementStack.popLast() }
-  }
-
-  public static func run(_ element: some ExampleElement) throws {
-    let run = ExampleRun()
-
-    if let current {
-      logger.error("running new element \"\(element.description)\" when already running \"\(current.description)\"")
-    }
-    try ExampleRun.$current.withValue(run) {
-      try element.execute(in: run)
-    }
-  }
-}
 
 public typealias Describe = ExampleGroup
 public typealias Context = ExampleGroup
