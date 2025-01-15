0dd2d08e6c15096fb7861f44ea033750bd74952b
David Catmull <davidcatmull@gmail.com>
2025-01-15 09:44:49 -0700

    Put ExampleGroup in a separate file

--- Sources/Rundown/ExampleData.swift
+++ Sources/Rundown/ExampleData.swift
@@ -56,87 +56,6 @@ public protocol ExampleElement: Element {
   var isDeepFocused: Bool { get }
 }
 
-public struct ExampleGroup: ExampleElement {
-  public let description: String
-  public let traits: [any Trait]
-  let beforeAll: [BeforeAll]
-  let beforeEach: [BeforeEach]
-  let afterEach: [AfterEach]
-  let afterAll: [AfterAll]
-  let elements: [any ExampleElement]
-  
-  public var isDeepFocused: Bool {
-    isFocused || elements.contains(where: \.isDeepFocused)
-  }
-
-  public init(_ description: String,
-              _ traits: (any Trait)...,
-              @ExampleBuilder builder: () -> ExampleGroup) {
-    self.init(description, traits, builder: builder)
-  }
-
-  public init(_ description: String,
-              _ traits: [any Trait],
-              @ExampleBuilder builder: () -> ExampleGroup)
-  {
-    let builtGroup = builder()
-    
-    self.description = description
-    self.traits = traits
-    self.beforeAll = builtGroup.beforeAll
-    self.beforeEach = builtGroup.beforeEach
-    self.afterEach = builtGroup.afterEach
-    self.afterAll = builtGroup.afterAll
-    self.elements = builtGroup.elements
-  }
-
-  internal init(_ description: String = "",
-                _ traits: [any Trait] = [],
-                elements: [any ExampleElement]) {
-    self.description = description
-    self.traits = traits
-    self.beforeAll = []
-    self.beforeEach = []
-    self.afterEach = []
-    self.afterAll = []
-    self.elements = elements
-  }
-
-  init(description: String,
-       traits: [any Trait],
-       beforeAll: [BeforeAll],
-       beforeEach: [BeforeEach],
-       afterEach: [AfterEach],
-       afterAll: [AfterAll],
-       elements: [any ExampleElement]) {
-    self.description = description
-    self.traits = traits
-    self.beforeAll = beforeAll
-    self.beforeEach = beforeEach
-    self.afterEach = afterEach
-    self.afterAll = afterAll
-    self.elements = elements
-  }
-  
-  /// Returns the group with a different name
-  public func named(_ name: String) -> Self {
-    return Self.init(description: name,
-                     traits: traits,
-                     beforeAll: beforeAll,
-                     beforeEach: beforeEach,
-                     afterEach: afterEach,
-                     afterAll: afterAll,
-                     elements: elements)
-  }
-  
-  public func run() throws {
-    try ExampleRun.run(self)
-  }
-  
-  public func execute(in run: ExampleRun) throws {
-    try run.run(self)
-  }
-}
 
 /// Example group that allows for callback-based setup and teardown, such as
 /// `TaskLocal.withValue()`
@@ -170,10 +89,6 @@ public struct Within: ExampleElement {
   }
 }
 
-
-
-public typealias Describe = ExampleGroup
-public typealias Context = ExampleGroup
 
 public struct It: ExampleElement {
   public let description: String
